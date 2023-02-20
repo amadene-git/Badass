cb81d1a608f7cd59b6e1325899b9697d85619369
amadene-git <adioumd@hotmail.fr>
2023-02-20 15:36:29 +0100

    lun. 20 févr. 2023 15:36:29 CET

--- P1/start.sh
+++ P1/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cat router_img.tar.gz | docker import --change 'CMD ["/sbin/tini", "--", "/usr/lib/frr/docker-start"]' - routeur_img
-cat host_img.tar | docker import - host_img
-
-gns3 P1.gns3
+docker pull alpine
+docker build . -t routeur_img
+gns3 P1-1.gns3
